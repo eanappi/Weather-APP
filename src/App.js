3cf4f8ab5386964ac2ac483fcbcfeb9f0fba70cc
Ernesto Alejandro Nappi <eanappi@gmail.com>
2019-09-19 10:42:22 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import AppHeader from './AppHeader.js'
+
 const App = () => {
   return `
     <section class="hero is-fullheight is-warning">
-      <div class="hero-head">
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-            <a class="navbar-item" href="/">
-              <img src="public/logo.png" alt="Weather New Style">
-            </a>
-          </div>
-        </nav>
-      </div>
+      
+      ${AppHeader}
 
       <div class="hero-body">
         <div class="container has-text-centered">
           <div class="columns">
-            <div class="column is-one-third"></div>
-            <div class="column is-one-third">
+            <div class="column"></div>
+            <div class="column">
               First column
             </div>
-            <div class="column is-one-third"></div>
+            <div class="column"></div>
           </div>
 
           <div class="columns">
---
+++ src/AppHeader.js
@@ -0,0 +1,15 @@
+const AppHeader = () => {
+  return `
+    <div class="hero-head">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <img src="public/logo.png" alt="Weather New Style">
+          </a>
+        </div>
+      </nav>
+    </div>
+  `
+}
+
+export default AppHeader()
