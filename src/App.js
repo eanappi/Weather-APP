ec5e3837b3e86ad3a0b369a6baf4f7c8af641596
Ernesto A. Nappi <eanappi@users.noreply.github.com>
2019-09-20 23:25:07 -0300

    In Progress ...

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ const App = () => {
         </div>
       </div>
     </section>
+    <div id="debug"></div>
   `
 }
 
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,8 +2,13 @@ const WeatherForecast = async () => {
   const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=3435907&lang=es&units=metric&appid=bbf8e3e4e4b29b00771fc2b9882c2114')
   const data = await response.json()
   
-  console.log(data.list.length)
+  console.log(data.list)
   
+  document.getElementById('debug').innerHTML = `
+  <pre class="is-size-4">
+    ${JSON.stringify(data.list, null, 2)}
+  </pre>
+  `
   
 }
 
