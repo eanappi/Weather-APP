2e6536fe6073b3c700fe6620f7e47d30ed694637
Ernesto A. Nappi <eanappi@users.noreply.github.com>
2019-09-20 22:17:29 -0300

    Refactor -- Remove component and rewrite into app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import AppHeader from './AppHeader.js'
 import WeatherDay from './WeatherDay.js'
 
 const App = () => {
   return `
     <section class="hero is-fullheight is-warning">
-      ${AppHeader}
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+              <img src="public/logo.png" alt="Weather New Style">
+            </a>
+          </div>
+        </nav>
+      </div>
       <div class="hero-body">
         <div class="container has-text-centered">
           <div class="columns">
             <div class="column"></div>
-            <div class="column" id="WeatherDay"></div>
+            <div class="column" id="WeatherDay">
+              <div class="is-loading"></div>
+            </div>
             <div class="column"></div>
           </div>
 
