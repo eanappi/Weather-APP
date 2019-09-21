dd38aa024ed391336f016f318cf3ffc84a378409
Ernesto A. Nappi <eanappi@users.noreply.github.com>
2019-09-20 23:31:49 -0300

    Debug one Forecast Weather card

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,13 +2,7 @@ const WeatherForecast = async () => {
   const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=3435907&lang=es&units=metric&appid=bbf8e3e4e4b29b00771fc2b9882c2114')
   const data = await response.json()
   
-  console.log(data.list)
-  
-  document.getElementById('debug').innerHTML = `
-  <pre class="is-size-4">
-    ${JSON.stringify(data.list, null, 2)}
-  </pre>
-  `
+  document.write(`<pre>${JSON.stringify(data.list[0], null, 2)}</pre>`)
   
 }
 
