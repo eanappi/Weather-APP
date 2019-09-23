40256ca0eaaa214c197e8038f187b11be847a682
Ernesto Alejandro Nappi <eanappi@gmail.com>
2019-09-23 13:16:37 -0300

    FIX - Days forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,8 @@
 const WeatherForecast = async () => {
-  const response = await fetch('https://api.openweathermap.org/data/2.5/forecast/daily?id=3435907&lang=es&units=metric&cnt=4&appid=bbf8e3e4e4b29b00771fc2b9882c2114')
+  const response = await fetch('https://api.openweathermap.org/data/2.5/forecast/daily?id=3435907&lang=es&units=metric&cnt=5&appid=bbf8e3e4e4b29b00771fc2b9882c2114')
   const data = await response.json()
 
-  const weekName = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+  const weekName = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
 
 //document.write(`<pre>${JSON.stringify(data.list[0], null, 2)}</pre>`)
 
@@ -38,7 +38,7 @@ const WeatherForecast = async () => {
 
 
   let template = `
-    ${data.list.map(forecastDaily => {
+    ${data.list.slice(1).map(forecastDaily => {
       let icon = forecastDaily.weather[0].icon
       let tempDay = forecastDaily.temp.day.toString().slice(0, -1)
       let tempMin = forecastDaily.temp.min.toString().slice(0, -1)
