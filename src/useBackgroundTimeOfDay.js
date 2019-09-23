529cd1a7400845373c388fdb6761971b4f8211aa
Ernesto A. Nappi <eanappi@users.noreply.github.com>
2019-09-23 20:38:12 -0300

    FIX -- Refactory and restructure of folders

--- src/useBackgroundTimeOfDay.js
+++ src/useBackgroundTimeOfDay.js
@@ -2,13 +2,13 @@ const useBackgroundTimeOfDay = () => {
   const hours = new Date().getHours()
 
   const timeOfDayClass = (
-    hours < 12 ? 'has-dawn-background'
+    hours <= 12 ? 'has-dawn-background'
     :
-    hours < 16 ? 'has-midday-background'
+    hours <= 16 ? 'has-midday-background'
     :
-    hours < 20 ? 'has-afternoon-background'
+    hours <= 20 ? 'has-afternoon-background'
     :
-    hours < 0 ? 'has-night-background' : ''
+    hours <= 0 ? 'has-night-background' : ''
   )
 
   return timeOfDayClass
